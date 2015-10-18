10793bf3da8077d70ad269df24a5edebb2bd038f
David Greminger <david.greminger@1up.io>
2015-10-18 12:07:46 +0200

    Config and DCA

---
+++ contao/dca/tl_mailchimp.php
@@ -0,0 +1,110 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_mailchimp'] = [
+    'config' => [
+        'dataContainer' => 'Table',
+        'enableVersioning' => true,
+        'sql' => [
+            'keys' => [
+                'id' => 'primary',
+            ],
+        ],
+    ],
+
+    'list' => [
+        'sorting' => [
+            'mode' => 2,
+            'fields' => ['listName'],
+            'flag' => 1,
+            'panelLayout' => 'sort,search,limit',
+        ],
+
+        'label' => [
+            'fields' => ['listName', 'listApiKey', 'listId'],
+            'format' => '%s',
+        ],
+
+        'global_operations' => [
+            'all' => [
+                'label' => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href' => 'act=select',
+                'class' => 'header_edit_all',
+                'attributes' => 'onclick="Backend.getScrollOffset()" accesskey="e"',
+            ],
+        ],
+
+        'operations' => [
+            'edit' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_mailchimp']['edit'],
+                'href' => 'act=edit',
+                'icon' => 'edit.gif',
+            ],
+
+            'delete' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_mailchimp']['delete'],
+                'href' => 'act=delete',
+                'icon' => 'delete.gif',
+                'attributes' => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"',
+            ],
+
+            'show' => [
+                'label' => &$GLOBALS['TL_LANG']['tl_mailchimp']['show'],
+                'href' => 'act=show',
+                'icon' => 'show.gif',
+                'attributes' => 'style="margin-right: 3px"',
+            ],
+        ],
+    ],
+
+    'palettes' => [
+        'default' => '{list_legend},listName,{mailchimp_legend},listApiKey,listId',
+    ],
+
+    'fields' => [
+        'id' => [
+            'sql' => "int(10) unsigned NOT NULL auto_increment",
+        ],
+
+        'tstamp' => [
+            'sql' => "int(10) unsigned NOT NULL default '0'",
+        ],
+
+        'listName' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_mailchimp']['listName'],
+            'exclude' => true,
+            'sorting' => true,
+            'search' => true,
+            'flag' => 1,
+            'filter' => true,
+            'inputType' => 'text',
+            'eval' => [
+                'mandatory' => true,
+                'unique' => true,
+                'maxlength' => 255
+            ],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+
+        'listApiKey' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_mailchimp']['listApiKey'],
+            'exclude' => true,
+            'inputType' => 'text',
+            'eval' => [
+                'mandatory' => true,
+                'maxlength' => 255
+            ],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+
+        'listId' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_mailchimp']['listId'],
+            'exclude' => true,
+            'inputType' => 'text',
+            'eval' => [
+                'mandatory' => true,
+                'maxlength' => 255
+            ],
+            'sql' => "varchar(255) NOT NULL default ''",
+        ],
+    ],
+];
