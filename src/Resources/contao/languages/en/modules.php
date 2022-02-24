cafbf34eaf338fdc3a53f95e38f7f8a1c118a0e9
David Greminger <bytehead@users.noreply.github.com>
2022-02-24 18:33:09 +0100

    Convert language files to php format (#36)

---
+++ src/Resources/contao/languages/en/modules.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+$GLOBALS['TL_LANG']['MOD']['mailchimp'] = ['MailChimp', 'MailChimp Subscribe/Unsubscribe Forms for Contao.'];
+$GLOBALS['TL_LANG']['FMD']['mailchimp'] = ['MailChimp'];
+$GLOBALS['TL_LANG']['FMD']['mailchimp_subscribe'] = ['Subscribe form', 'Generates a MailChimp subscribe form.'];
+$GLOBALS['TL_LANG']['FMD']['mailchimp_unsubscribe'] = ['Unsubscribe form', 'Generates a MailChimp unsubscribe form.'];
